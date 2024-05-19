7bcd34f82102a0e35f60bdde6c4dca788521ebc5
Tlylight <21521340@gm.uit.edu.vn>
2024-05-20 02:20:35 +0700

    Used sfml lib

--- Mario.cpp
+++ Mario.cpp
@@ -1,9 +1,22 @@
+// Mario.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
 #include <iostream>
-
+#include "SFML/Graphics.hpp"
 int main()
 {
     std::cout << "Hello World!\n";
-}
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Test");
+    sf::Event e;
+    
+    while (window.isOpen())
+    {
+        while (window.pollEvent(e))
+        {
+            if (e.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 
+}
 
