9530ea00e5fbd9fee4a9b725685f83a5b04a311a
Tlylight <21521340@gm.uit.edu.vn>
2024-05-21 00:05:37 +0700

    implemented Window stretching with correct aspect ratio

--- Mario.cpp
+++ Mario.cpp
@@ -1,6 +1,6 @@
 // Mario.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+/*
 #include <iostream>
 #include "SFML/Graphics.hpp"
 int main()
@@ -19,4 +19,37 @@ int main()
     }
 
 }
+*/
+#include <SFML/Graphics.hpp>
+#include "game.h"
+#include "camera.h"
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1200, 900), "Mario World");
+    sf::Clock deltaClock;
+    Camera camera;
 
+
+    Begin(window);
+    while (window.isOpen()) 
+    {
+        float deltaTime = deltaClock.restart().asSeconds();
+
+        sf::Event event{};
+        while (window.pollEvent(event)) 
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.setView(camera.GetView(window.getSize()));
+        Update(deltaTime);
+       
+        window.clear();
+
+        Render(window);
+
+        window.display();
+
+    }
+}
